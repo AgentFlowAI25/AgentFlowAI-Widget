5678d87dcb2fe21daad2fdbb7b73d9c3547c9d00
AgentFlowAI25 <kontakt@agentflowai.de>
2025-05-28 12:50:33 +0200

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,25 +1,20 @@
+@@ -0,0 +1,19 @@
 (function () {
-  const cssUrl = "https://deine-netlify-domain.netlify.app/widget.css";
-  const jsUrl = "https://deine-netlify-domain.netlify.app/widget.js";
-  const containerId = "agentflowai-chat-widget";
-
-  // Verhindern, dass das Widget doppelt geladen wird
-  if (document.getElementById(containerId)) return;
-
-  // Widget-Container erstellen
-  const container = document.createElement("div");
-  container.id = containerId;
-  document.body.appendChild(container);
-
-  // CSS laden
   const link = document.createElement("link");
   link.rel = "stylesheet";
-  link.href = cssUrl;
+  link.href = "https://<AgentFlowAI25>.github.io/<AgentFlowAI-Widget>/widget.css";
   document.head.appendChild(link);
 
-  // JS laden
-  const script = document.createElement("script");
-  script.src = jsUrl;
-  script.defer = true;
-  document.body.appendChild(script);
+  fetch("https://<AgentFlowAI25>.github.io/<AgentFlowAI-Widget>/index.html")
+    .then(res => res.text())
+    .then(html => {
+      const wrapper = document.createElement("div");
+      wrapper.innerHTML = html;
+      document.body.appendChild(wrapper);
+
+      const script = document.createElement("script");
+      script.src = "https://<AgentFlowAI25>.github.io/<AgentFlowAI-Widget>/widget.js";
+      script.defer = true;
+      document.body.appendChild(script);
+    });
 })();
